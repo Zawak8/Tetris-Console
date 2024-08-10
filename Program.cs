db41b2511de4a4326812f9548b688d0b21c26029
ВикторАДиндев <viktordindev@gmail.com>
2024-08-10 11:39:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Tetris_Console
+{
+    internal class Program
+    {
+        static int TetrisRows = 20;
+        static int TetrisCols = 10;
+        static int InfoCols = 10;
+        static int ConsoleRows = 1 + TetrisRows + 1;
+        static int ConsoleCols = 1 + TetrisCols + 1 + InfoCols + 1;
+        static void Main(string[] args)
+        {
+            Console.Title = "Tetris v1.0";
+            Console.WindowHeight = ConsoleRows;
+            Console.WindowWidth = ConsoleCols;
+            Console.ReadKey();
+
+        }
+
+        static void Write(string text, int row, int col, ConsoleColor color = ConsoleColor.Yellow)
+        {
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(col, row);
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+    }
+}
